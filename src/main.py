0ab417091699130640b9a1400782059b55f4d4ae
dnutiu <denis.nutiu@gmail.com>
2024-01-26 20:00:06 +0200

    fix settings game_name

--- src/main.py
+++ src/main.py
@@ -7,7 +7,8 @@ from src.bot.discord.bot import NucuBot
 def main():
     settings = Settings()
     bot = NucuBot.create(
-        command_prefix=settings.discord.command_prefix, game_name=settings.game_name
+        command_prefix=settings.discord.command_prefix,
+        game_name=settings.discord.game_name,
     )
     bot.run(settings.discord.token)
 
